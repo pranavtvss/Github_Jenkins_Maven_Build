b2d09b8a3435c34dee590f50c0cb544f775f364f
pranavtvss <pranav@techvision.net.in>
2018-05-16 18:17:19 +0530

    Update TestNgMavenExampleTest.java

--- src/test/TestNgMavenExampleTest.java
+++ src/test/TestNgMavenExampleTest.java
@@ -7,7 +7,8 @@ public class TestNgMavenExampleTest {
 
 	@Test(priority = 0)
 	public void testStringCompare() {
-		System.out.println("Testing test with priority 0");
+		System.out.println("Testing class TestNgMavenExampleTest with priority 0 ");
+		System.out.println("Testing function with priority 0");
 		
 		String s = "initial";
 		AssertJUnit.assertEquals(s, "initial");
@@ -16,7 +17,7 @@ public class TestNgMavenExampleTest {
 	
 	@Test(priority = 1)
 	public void testNullablity() {
-		System.out.println("Testing test with priority 1");
+		System.out.println("Testing function with priority 1");
 		
 		String s = " ";
 		AssertJUnit.assertNotNull(s);
