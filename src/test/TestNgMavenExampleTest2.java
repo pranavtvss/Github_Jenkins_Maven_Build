1a0a94dee06a2dfee72c79d373680257939284bc
pranavtvss <pranav@techvision.net.in>
2018-05-16 18:18:03 +0530

    Update TestNgMavenExampleTest2.java

--- src/test/TestNgMavenExampleTest2.java
+++ src/test/TestNgMavenExampleTest2.java
@@ -9,7 +9,8 @@ public class TestNgMavenExampleTest2 {
 	
 	@Test
 	public void testEquality() {
-		System.out.println("This is TestNG-Maven Example");
+		System.out.println("Testing class TestNgMavenExampleTest2 with priority 1 ");
+		System.out.println("Testing function with priority 2");
 		
 		String s = " ";
 		AssertJUnit.assertEquals(" ",s);
