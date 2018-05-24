60d18bce33710a47cd555025e6f02e1dc041e5f6
pranavtvss <pranav@techvision.net.in>
2018-05-24 18:01:00 +0530

    Update qunit-reporter-junit.test.js

--- src/test/qunit-reporter-junit.test.js
+++ src/test/qunit-reporter-junit.test.js
@@ -3,33 +3,16 @@
 	module('Module 1');
 
 	test('test 1', function(assert) {
-		assert.expect(2);
-		assert.equal(1, 1, 'Assert 1 = 1');
+		//assert.expect(2);
+		//assert.equal(1, 1, 'Assert 1 = 1');
 		
 	});
 
 	test('test 2', function(assert) {
-		assert.expect(3);
-		assert.equal(1, 1, 'Assert 1 = 1');
-	
-		assert.equal(1, 1, 'Assert 1 = 1');
+		//assert.expect(3);
+		//assert.equal(1, 1, 'Assert 1 = 1');
+		//assert.equal(1, 1, 'Assert 1 = 1');
 	});
-
-
-	module('Module 2');
-
-	test('test 3', function(assert) {
-		assert.expect(2);
-		assert.equal(1, 1, 'Assert 1 = 1');
 	
-	});
-
-	test('test 4', function(assert) {
-		assert.expect(3);
-		assert.equal(1, 1, 'Assert 1 = 1');
-	
-		assert.equal(1, 3, 'Assert fail 1 = 3');
-	});
-
 
 })( QUnit.module, QUnit.test );
