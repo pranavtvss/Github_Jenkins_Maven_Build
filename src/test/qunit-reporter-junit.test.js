d80630c5d5c8237804b0e6f34fffc32df8cfd3b5
pranavtvss <pranav@techvision.net.in>
2018-05-24 18:02:37 +0530

    passing tests added

--- src/test/qunit-reporter-junit.test.js
+++ src/test/qunit-reporter-junit.test.js
@@ -3,15 +3,15 @@
 	module('Module 1');
 
 	test('test 1', function(assert) {
-		//assert.expect(2);
-		//assert.equal(1, 1, 'Assert 1 = 1');
+		
+		assert.equal(1, 1, 'Assert 1 = 1');
 		
 	});
 
 	test('test 2', function(assert) {
-		//assert.expect(3);
-		//assert.equal(1, 1, 'Assert 1 = 1');
-		//assert.equal(1, 1, 'Assert 1 = 1');
+	
+		assert.equal(1, 1, 'Assert 1 = 1');
+		assert.equal(1, 1, 'Assert 1 = 1');
 	});
 	
 
