9508406d5f135c171b09c2edf59dce298b6cbd49
pranavtvss <pranav@techvision.net.in>
2018-05-24 17:55:13 +0530

    Update qunit-reporter-junit.test.js

--- src/test/qunit-reporter-junit.test.js
+++ src/test/qunit-reporter-junit.test.js
@@ -5,13 +5,13 @@
 	test('test 1', function(assert) {
 		assert.expect(2);
 		assert.equal(1, 1, 'Assert 1 = 1');
-		assert.equal(1, 2, 'Assert fail 1 = 2');
+		
 	});
 
 	test('test 2', function(assert) {
 		assert.expect(3);
 		assert.equal(1, 1, 'Assert 1 = 1');
-		assert.equal(1, 2, 'Assert fail 1 = 2');
+	
 		assert.equal(1, 1, 'Assert 1 = 1');
 	});
 
@@ -21,13 +21,13 @@
 	test('test 3', function(assert) {
 		assert.expect(2);
 		assert.equal(1, 1, 'Assert 1 = 1');
-		assert.equal(1, 2, 'Assert fail 1 = 2');
+	
 	});
 
 	test('test 4', function(assert) {
 		assert.expect(3);
 		assert.equal(1, 1, 'Assert 1 = 1');
-		assert.equal(1, 2, 'Assert fail 1 = 2');
+	
 		assert.equal(1, 3, 'Assert fail 1 = 3');
 	});
 
